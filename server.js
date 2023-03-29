00b91644133e3ef4d1f33588385d1561ef63f861
adamgulde <adam.j.gulde@gmail.com>
2023-03-29 11:09:56 -0400

    adding node.js database!

---
+++ server.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2/promise");
+
+const insertIntoDB = async (base64String, returnString) => {
+    const connection = await mysql.createConnection({
+        host: "sql9.freesqldatabase.com",
+        user: "sql9609574",
+        password: "U6JqdflMxh",
+        database: "sql9609574",
+        port:3306,
+    })
+
+    try {
+        await connection.query(
+            "INSERT INTO project4 (base64String, returnString) VALUES ('"+base64String+"', '"+returnString+"')"
+        );
+        console.log("Inserted ("+base64String+","+returnString+") into DB")
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+insertIntoDB("lognnggoengonegoengonegoeogngo", "this is fake")
